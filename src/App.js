33b030ef3ddd79fc2c25b81e759577c8dffe2444
ealtugkirman <110311214+ealtugkirman@users.noreply.github.com>
2022-12-02 05:27:51 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -18,7 +18,7 @@ const App = () => {
       <h3>Learn More</h3>
 
       <FlipClockCountdown
-        to={new Date().getTime() + 24 * 3600 * 1000 + 5000}
+         to={new Date("2023-03-25").getTime() + 24 * 3600 * 1000 + 5000}
         className='flip-clock'
         labels={['DAYS', 'HOURS', 'MINUTES', 'SECONDS']}
         duration={0.5}

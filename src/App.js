bef4b149937cdb317053dda15c29518d5493c30f
ealtugkirman <110311214+ealtugkirman@users.noreply.github.com>
2022-12-02 06:27:08 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -13,17 +13,20 @@ const App = () => {
     <video src={VideoBg} autoPlay loop muted></video>
     <div className="page__content">
       <h1>
-        Launching Soon
+        CoalUnion 
       </h1>
-      <h3>Learn More</h3>
+      <h2>Launching Soon!</h2>
+      <img class="logo" src="https://cdn.glitch.global/5e8af274-91e7-4fb5-97f4-c5839d809c90/logo-nobg-v1.0.png?v=1669643929046" alt="CoalUnion-logo"></img>
+
+      {/* <h3>Learn More</h3> */}
 
       <FlipClockCountdown
-         to={new Date("2023-03-25").getTime() + 24 * 3600 * 1000 + 5000}
+        to={new Date("2023-02-25").getTime() + 24 * 3600 * 1000 + 5000}
         className='flip-clock'
         labels={['DAYS', 'HOURS', 'MINUTES', 'SECONDS']}
         duration={0.5}
       />
-      <button className="btn"> <a href="https://google.com">Learn More</a>  </button>
+      <button className="btn"> <a href="https://twitter.com/coalunion">Get Ready</a>  </button>
     </div>
     
     </section >;
